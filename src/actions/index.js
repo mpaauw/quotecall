82da31568afd612f30ff5d0d05f265e8f8074212
adjutant-xx <matt.paauw@gmail.com>
2016-12-08 12:42:30 -0800

    Fixed a bug within the actions/index.js file where the PROTECTED_KEY variable imported from config.js in root would not be properly recognized.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import PROTECTED_KEY from '../../config';
+import { PROTECTED_KEY } from '../../config';
 
 const API_KEY = PROTECTED_KEY; // Quandl API Key omitted for security reasons, visit the URL below to create your own.
 const ROOT_URL = `https://www.quandl.com/api/v3/datasets/WIKI/`; // stock quotes are pulled from: https://www.quandl.com/docs/api#introduction
