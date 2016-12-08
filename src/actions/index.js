044c9a4813c14e2b245f35cfeb10cf77d1e3aa8e
adjutant-xx <matt.paauw@gmail.com>
2016-12-08 12:34:10 -0800

    Added ROOT_URL variable to precede the url string value in actions/index.js.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ const ROOT_URL = `https://www.quandl.com/api/v3/datasets/WIKI/`; // stock quotes
 export const FETCH_QUOTE = 'FETCH_QUOTE';
 
 export function fetchQuote(term){
-    const url = `${term}.json?api_key=${API_KEY}`;
+    const url = `${ROOT_URL}${term}.json?api_key=${API_KEY}`;
     const request = axios.get(url);
 
     return {
