14bb1dc82b537e8ecfcdafd47ad8d9c6cfc89a50
adjutant-xx <matt.paauw@gmail.com>
2016-12-08 08:02:39 -0800

    Added a 'SearchBar' class to the 'search_bar.js' file.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,3 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event){
+        event.preventDefault(); 
+    
+        //this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
+}
+
+    render(){
+        return(
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input 
+                    placeholder="search a stock quote"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
