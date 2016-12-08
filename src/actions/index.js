f356f36fa3ded1e14cb8dc2da7ce691e8629cf13
adjutant-xx <matt.paauw@gmail.com>
2016-12-08 09:27:50 -0800

    Added code to actions/index.js to securely retrieve an API key from application's config.json file.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
-const API_KEY = ''; // Quandl API Key omitted for security reasons, visit the URL below to create your own.
+const API_KEY = getAPIKey(); // Quandl API Key omitted for security reasons, visit the URL below to create your own.
 const ROOT_URL = `https://www.quandl.com/api/v3/datasets/WIKI/`; // stock quotes are pulled from: https://www.quandl.com/docs/api#introduction
 
 export const FETCH_QUOTE = 'FETCH_QUOTE';
 
+function getAPIKey(){
+    var fs, config;
+    config = 'config.json';
+    fs = require('fs');
+
+    var readFile = JSON.parse(fs.readFileSync(config));
+
+    return readFile.key;
+}
+
 export function fetchQuote(term){
+
     const url = `${term}.json?api_key=${API_KEY}`;
     const request = axios.get(url);
 
