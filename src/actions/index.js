e5efd99890a4776a3ce53b2e948a76d6b31325b1
adjutant-xx <matt.paauw@gmail.com>
2016-12-08 09:04:41 -0800

    Added initial axios API-calling code with API_KEY ommitted to src/actions/index.js; pushing this initial file, will add to .gitignore in future commits.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,16 @@
 import axios from 'axios';
 
-const API_KEY = '';
-const ROOT_URL = ``;
+const API_KEY = ''; // Quandl API Key omitted for security reasons, visit the URL below to create your own.
+const ROOT_URL = `https://www.quandl.com/api/v3/datasets/WIKI/`; // stock quotes are pulled from: https://www.quandl.com/docs/api#introduction
+
+export const FETCH_QUOTE = 'FETCH_QUOTE';
+
+export function fetchQuote(term){
+    const url = `${term}.json?api_key=${API_KEY}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_QUOTE,
+        payload: request
+    };
+}
