3d405bea9ff456c8bbb7c974695c161ca2a7e190
adjutant-xx <matt.paauw@gmail.com>
2016-12-09 11:55:59 -0800

    Refactored the 'today()' method within the 'actions/date.js' file.

--- src/actions/date.js
+++ src/actions/date.js
@@ -1,15 +1,15 @@
 export function today(){
     var currentDate = new Date();
     var day = currentDate.getDate();
-    var month = currentDate.getMonth()+1; 
+    var month = currentDate.getMonth() + 1; 
     var year = currentDate.getFullYear();
 
-    if(day<10) {
-        day='0'+day
+    if(day < 10) {
+        day = '0' + day
     } 
 
-    if(month<10) {
-        month='0'+month
+    if(month < 10) {
+        month = '0'+month
     } 
 
     currentDate = year + month + day;
