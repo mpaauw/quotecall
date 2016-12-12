0857fe359ea05e241dbd36eaf1c0bad0549641a2
adjutant-xx <matt.paauw@gmail.com>
2016-12-12 12:58:30 -0800

    Imported ReduxPromise libraries into the 'src/index.js' file.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
