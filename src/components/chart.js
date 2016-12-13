d7b04f1d72a4a019e4da3a4b633a47da7c2ca064
adjutant-xx <matt.paauw@gmail.com>
2016-12-12 17:27:19 -0800

    Fixed a syntax bug within components/chart.js.

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,7 +4,7 @@ import highstock from 'highcharts';
 export default (props) => {
     return (
         <div>
-            <highstock data={props.data} config={props.config}>
+            <highstock data={props.parsedData} config={props.config}>
             </highstock>
         </div>
     );
