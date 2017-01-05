9cc255fd3b4becd2dd850d5cd568421106fc80fb
adjutant-xx <matt.paauw@gmail.com>
2017-01-04 21:01:19 -0800

    Removed unnecessary whitespace & newlines within src/containers/quote_display.js.

--- src/containers/quote_display.js
+++ src/containers/quote_display.js
@@ -23,13 +23,11 @@ class QuoteDisplay extends Component {
     renderQuoteDisplay(quoteData){
         if(quoteData.length === 0) {
             return;
-        } else {
- 
+        } else { 
             const name = quoteData[0].dataset.name.split(' (')[0];            
             const symbol = quoteData[0].dataset.dataset_code;
             const date = quoteData[0].dataset.data[0][responseColumns.date];
             const rawData = quoteData[0].dataset.data;
- 
             var parsedData = [];
  
             for(var i = 0; i < rawData.length; i++){
