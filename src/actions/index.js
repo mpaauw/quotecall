41cb65129d59b5c7461c0d1892b8f47c48f215dd
adjutant-xx <matt.paauw@gmail.com>
2017-01-07 10:10:53 -0800

    Removing calls to date function within the src/actions/index.js file, re-pushing to Heroku.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,16 +7,8 @@ const ROOT_URL = 'https://www.quandl.com/api/v3/datasets/WIKI/'; // stock quotes
 export const FETCH_QUOTE = 'FETCH_QUOTE';
 
 export function fetchQuote(term){
-
-    var frontDate = getCurrentDate(0); // today
-    var backDate = getCurrentDate(1); // pull all stock data up to a year back
-
-    //const url = `${ROOT_URL}${term}.json?start_date=${backDate}&end_date=${frontDate}&api_key=${PROTECTED_KEY}`;
-    const url = `${ROOT_URL}${term}.json?api_key=${PROTECTED_KEY}`; // test
-
-
+    const url = `${ROOT_URL}${term}.json?api_key=${PROTECTED_KEY}`;
     const request = axios.get(url);
-
     return {
         type: FETCH_QUOTE,
         payload: request
