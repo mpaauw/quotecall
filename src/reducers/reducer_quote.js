93cda33e224761e27f617edd80dbaa493164d5c9
adjutant-xx <matt.paauw@gmail.com>
2017-01-09 13:28:15 -0800

    Reverting.

--- src/reducers/reducer_quote.js
+++ src/reducers/reducer_quote.js
@@ -1,12 +1,9 @@
-import {FETCH_QUOTE, FETCH_QUOTE_FAIL} from '../actions/index';
+import {FETCH_QUOTE} from '../actions/index';
 
 export default function(state = [], action){
     switch(action.type){
         case FETCH_QUOTE:
             return [action.payload.data, ...state];
-        case FETCH_QUOTE_FAIL:
-            return state;
-        default: 
-            return state;
     }
+    return state;
 }
