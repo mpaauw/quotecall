01c9152425efefec9e4c2e066fdd1b29348a7324
adjutant-xx <matt.paauw@gmail.com>
2017-01-09 13:18:09 -0800

    Adding additional API call switch cases to reducer.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,13 @@ import { PROTECTED_KEY } from '../../config'; // Quandl API Key omitted for secu
 const ROOT_URL = 'https://www.quandl.com/api/v3/datasets/WIKI/'; // stock quotes are pulled from: https://www.quandl.com/docs/api#introduction
 
 export const FETCH_QUOTE = 'FETCH_QUOTE';
+export const FETCH_QUOTE_FAIL = 'FETCH_QUOTE_FAIL';
 
 export function fetchQuote(term){
     const url = `${ROOT_URL}${term}.json?api_key=${PROTECTED_KEY}`;
+
     const request = axios.get(url);
+    
     return {
         type: FETCH_QUOTE,
         payload: request
--- src/reducers/reducer_quote.js
+++ src/reducers/reducer_quote.js
@@ -1,9 +1,12 @@
-import {FETCH_QUOTE} from '../actions/index';
+import {FETCH_QUOTE, FETCH_QUOTE_FAIL} from '../actions/index';
 
 export default function(state = [], action){
     switch(action.type){
         case FETCH_QUOTE:
             return [action.payload.data, ...state];
+        case FETCH_QUOTE_FAIL:
+            return state;
+        default: 
+            return state;
     }
-    return state;
 }
