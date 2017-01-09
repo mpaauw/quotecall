62ccba6726ead6c70d41e3c919f15eab10342555
adjutant-xx <matt.paauw@gmail.com>
2017-01-09 13:59:43 -0800

    Testing more troubleshooting methods.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ import { PROTECTED_KEY } from '../../config'; // Quandl API Key omitted for secu
 const ROOT_URL = 'https://www.quandl.com/api/v3/datasets/WIKI/'; // stock quotes are pulled from: https://www.quandl.com/docs/api#introduction
 
 export const FETCH_QUOTE = 'FETCH_QUOTE';
+export const FETCH_QUOTE_FAIL = 'FETCH_QUOTE_FAIL';
 
 export function fetchQuote(term){
 
@@ -14,9 +15,20 @@ export function fetchQuote(term){
     }
 
     const url = `${ROOT_URL}${term}.json?api_key=${PROTECTED_KEY}`;
-    const request = axios.get(url);
-    return {
-        type: FETCH_QUOTE,
-        payload: request
-    };
+
+    const request = axios.get(url)
+        .then(response => {
+            return {
+                type: FETCH_QUOTE,
+                payload: response
+            };
+        })
+        .catch(error =>{
+            return {
+                type: FETCH_QUOTE_FAIL,
+                payload: error
+            };
+        });
+
+    
 }
